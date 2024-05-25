ee76ddda0ba47bc49c222d5e25878d937ef7ff6e
anirudh-bukka <anirudh.bukka@gmail.com>
2024-05-25 12:55:33 +0530

    Conditional

--- Shell/Conditional.sh
+++ Shell/Conditional.sh
@@ -1,1 +1,21 @@
-# Conditional
+#!/bin/bash
+
+# # Basic conditional.
+day="Monday"
+
+case $day in 
+    "Monday")
+        echo "Frontend"
+        ;;
+    "Friday")
+        echo "Backend"
+        ;;
+    "Saturday" | "Sunday")
+        echo "Weekend"
+        ;;
+    *)
+        echo "Pull stack - stackoverflow"
+        ;;
+esac
+
+# # Shell script to execute commands on pg-box
